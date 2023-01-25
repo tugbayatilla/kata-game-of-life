31580cb4cb8cea9c0bceb546501fef552d450c86
Tugbay Atilla <tatilla@igus.net>
2023-01-25 13:45:34 +0100

    refactor: one by one universe name is now _universeOneByOne

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -3,31 +3,31 @@ namespace kata_game_of_life_tests;
 public class UnitTest1
 {
     private const int Dead = 0;
-    private readonly Universe _universe = new Universe(1,1);
+    private readonly Universe _universeOneByOne = new Universe(1,1);
 
     [Fact]
     public void UniverseExist()
     {
-        Assert.NotNull(_universe);
+        Assert.NotNull(_universeOneByOne);
     }
     
     [Fact]
     public void Universe_has_two_dimensions()
     {
-        Assert.Equal(2, _universe.Dimensions.Rank);
+        Assert.Equal(2, _universeOneByOne.Dimensions.Rank);
     }
     
     [Fact]
     public void Universe_has_dimensions_are_one_by_one()
     {
-        Assert.Equal(1, _universe.Dimensions.GetLength(0));
-        Assert.Equal(1, _universe.Dimensions.GetLength(1));
+        Assert.Equal(1, _universeOneByOne.Dimensions.GetLength(0));
+        Assert.Equal(1, _universeOneByOne.Dimensions.GetLength(1));
     }
     
     [Fact]
     public void One_by_one_universe_has_a_dead_cell_at_the_beginning()
     {
-        Assert.Equal(Dead, _universe.Dimensions[0,0]);
+        Assert.Equal(Dead, _universeOneByOne.Dimensions[0,0]);
     }
 
 }
